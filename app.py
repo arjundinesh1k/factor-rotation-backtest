825657353eedeb59cef5c6578fcc97cfb0bff829
Arjun Dinesh <arjun.dinesh.1k@gmail.com>
2025-07-15 11:40:50 -0500

    Initial commit

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import plotly.graph_objs as go
 import plotly.io as pio
 from datetime import datetime, timedelta
 import time
+import os
 
 app = Flask(__name__)
 
@@ -108,4 +109,5 @@ def index():
     return render_template("index.html", plot_div=plot_div)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
